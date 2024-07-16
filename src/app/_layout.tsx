4f472dd946ee3931f6833e4e63fd361bd66f885a
Ongchen Sherpa <fullstacksherpa@gmail.com>
2024-07-15 18:35:38 -0700

    config expo router

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,5 @@
 import {Tabs} from 'expo-router';
 import React from 'react';
-
-import {TabBarIcon} from '@/components/navigation/TabBarIcon';
 import {Colors} from '@/constants/Colors';
 import {useColorScheme} from '@/hooks/useColorScheme';
 import {MaterialIcons} from '@expo/vector-icons';
@@ -16,7 +14,7 @@ export default function TabLayout() {
         headerShown: false,
       }}>
       <Tabs.Screen
-        name="index"
+        name="allocations"
         options={{
           title: 'Allocations',
           tabBarIcon: ({color, size}) => (
@@ -31,15 +29,6 @@ export default function TabLayout() {
           title: 'Accounts',
           tabBarIcon: ({color, size}) => (
             <MaterialIcons name='account-balance-wallet' size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({color, focused}) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
           ),
         }}
       />
---
+++ src/app/allocations/new-allocation.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native'
+
+export default function NewAllocationScreen() {
+	return (
+		<View>
+			<Text>New Allocation</Text>
+		</View>
+	)
+}
